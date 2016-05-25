479ecc06fac2b1590c40a1163d2a2a7bbc56882b
ststeiger <quandary82@hailmail.net>
2016-05-25 09:08:11 +0200

    Added LOB-Dataset

--- LargeObjectSerializer/ajax/serialize.ashx.cs
+++ LargeObjectSerializer/ajax/serialize.ashx.cs
@@ -13,9 +13,8 @@ namespace LargeObjectSerializer.ajax
     {
 
 
-        public void ProcessRequest(HttpContext context)
+        public static string GetConnectionString()
         {
-            context.Response.ContentType = "text/plain";
 
             System.Data.SqlClient.SqlConnectionStringBuilder csb = new System.Data.SqlClient.SqlConnectionStringBuilder();
 
@@ -23,8 +22,133 @@ namespace LargeObjectSerializer.ajax
             csb.InitialCatalog = "HBD_CAFM3_Produktiv";
             csb.IntegratedSecurity = true;
 
+            return csb.ConnectionString;
+        }
 
 
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+
+            if (true)
+                SerializeLargeDataset(context);
+            else
+                SerializeLargeTable(context);
+
+
+        }
+
+
+        public void SerializeLargeDataset(HttpContext context)
+        {
+            string strSQL = @"
+SELECT TOP 10 * FROM T_Benutzer; 
+SELECT TOP 10 * FROM T_Benutzergruppen; 
+
+-- SELECT * FROM T_Benutzer LIMIT 10; 
+-- SELECT * FROM T_Benutzergruppen LIMIT 10; 
+
+-- SELECT * FROM T_Benutzer OFFSET 0 FETCH NEXT 10 ROWS ONLY;
+-- SELECT * FROM T_Benutzergruppen OFFSET 0 FETCH NEXT 10 ROWS ONLY; 
+";
+
+            Newtonsoft.Json.JsonSerializer ser = new Newtonsoft.Json.JsonSerializer();
+
+            using (Newtonsoft.Json.JsonTextWriter jsonWriter = new Newtonsoft.Json.JsonTextWriter(context.Response.Output))
+            {
+                jsonWriter.Formatting = Newtonsoft.Json.Formatting.Indented;
+
+
+                jsonWriter.WriteStartObject();
+
+                jsonWriter.WritePropertyName("Tables");
+                jsonWriter.WriteStartArray();
+
+
+                using (System.Data.SqlClient.SqlConnection con = new System.Data.SqlClient.SqlConnection(GetConnectionString()))
+                {
+                    if (con.State != System.Data.ConnectionState.Open)
+                        con.Open();
+
+                    using (System.Data.SqlClient.SqlCommand cmd = con.CreateCommand())
+                    {
+                        cmd.CommandText = strSQL;
+
+                        using (System.Data.SqlClient.SqlDataReader dr = cmd.ExecuteReader(System.Data.CommandBehavior.SequentialAccess
+                             | System.Data.CommandBehavior.CloseConnection
+                            ))
+                        {
+
+                            do
+                            {
+                                jsonWriter.WriteStartObject(); // tbl = new Table();
+
+                                jsonWriter.WritePropertyName("Columns");
+                                jsonWriter.WriteStartArray();
+
+
+                                for (int i = 0; i < dr.FieldCount; ++i)
+                                {
+                                    jsonWriter.WriteStartObject();
+
+                                    jsonWriter.WritePropertyName("ColumnName");
+                                    jsonWriter.WriteValue(dr.GetName(i));
+
+                                    jsonWriter.WritePropertyName("FieldType");
+                                    jsonWriter.WriteValue(GetAssemblyQualifiedNoVersionName(dr.GetFieldType(i)));
+
+                                    jsonWriter.WriteEndObject();
+                                } // Next i 
+                                jsonWriter.WriteEndArray();
+
+                                jsonWriter.WritePropertyName("Rows");
+                                jsonWriter.WriteStartArray();
+
+                                if (dr.HasRows)
+                                {
+
+                                    while (dr.Read())
+                                    {
+                                        object[] thisRow = new object[dr.FieldCount];
+
+                                        jsonWriter.WriteStartArray(); // object[] thisRow = new object[dr.FieldCount];
+                                        for (int i = 0; i < dr.FieldCount; ++i)
+                                        {
+                                            jsonWriter.WriteValue(dr.GetValue(i));
+                                        } // Next i
+                                        jsonWriter.WriteEndArray(); // tbl.Rows.Add(thisRow);
+                                    } // Whend 
+
+                                } // End if (dr.HasRows) 
+
+                                jsonWriter.WriteEndArray();
+
+                                jsonWriter.WriteEndObject(); // ser.Tables.Add(tbl);
+                            } while (dr.NextResult()); 
+                           
+                        } // End using dr 
+
+                    } // End using cmd 
+
+                    
+                    if (con.State != System.Data.ConnectionState.Closed)
+                        con.Close();
+                } // End using con 
+
+                jsonWriter.WriteEndArray();
+
+                jsonWriter.WriteEndObject();
+                jsonWriter.Flush();
+            } // End Using jsonWriter 
+
+            context.Response.Output.Flush();
+            context.Response.OutputStream.Flush();
+            context.Response.Flush();
+        } // End Sub SerializeLargeDataset 
+
+
+        public void SerializeLargeTable(HttpContext context)
+        {
             Newtonsoft.Json.JsonSerializer ser = new Newtonsoft.Json.JsonSerializer();
 
             using (Newtonsoft.Json.JsonTextWriter jsonWriter = new Newtonsoft.Json.JsonTextWriter(context.Response.Output))
@@ -34,7 +158,7 @@ namespace LargeObjectSerializer.ajax
                 jsonWriter.WriteStartObject();
 
 
-                using (System.Data.SqlClient.SqlConnection con = new System.Data.SqlClient.SqlConnection(csb.ConnectionString))
+                using (System.Data.SqlClient.SqlConnection con = new System.Data.SqlClient.SqlConnection(GetConnectionString()))
                 {
                     if (con.State != System.Data.ConnectionState.Open)
                         con.Open();
@@ -65,7 +189,7 @@ namespace LargeObjectSerializer.ajax
                                 jsonWriter.WriteValue(colName);
 
                                 jsonWriter.WritePropertyName("DataType");
-                                jsonWriter.WriteValue(GetAssemblyQualifiedNoVersionName(t.AssemblyQualifiedName));
+                                jsonWriter.WriteValue(GetAssemblyQualifiedNoVersionName(t));
 
                                 // jsonWriter.WritePropertyName("DateTimeMode");
                                 // jsonWriter.WriteValue(column.DateTimeMode.ToString());
@@ -114,20 +238,26 @@ namespace LargeObjectSerializer.ajax
                         con.Close();
                 } // End using con 
 
-
                 
                 jsonWriter.WriteEndObject();
 
                 jsonWriter.Flush();
                 context.Response.Output.Flush();
+                context.Response.OutputStream.Flush();
                 context.Response.Flush();
             } // End Using jsonWriter 
 
-
-        }
-
+        } // End Sub SerializeLargeTable 
 
 
+
+        public static string GetAssemblyQualifiedNoVersionName(System.Type type)
+        {
+            if (type == null)
+                return null;
+
+            return GetAssemblyQualifiedNoVersionName(type.AssemblyQualifiedName);
+        } // End Function GetAssemblyQualifiedNoVersionName
 
 
         public static string GetAssemblyQualifiedNoVersionName(string input)
@@ -146,7 +276,7 @@ namespace LargeObjectSerializer.ajax
             }
 
             return input.Substring(0, i);
-        }
+        } // End Function GetAssemblyQualifiedNoVersionName
 
 
         public bool IsReusable
@@ -155,6 +285,10 @@ namespace LargeObjectSerializer.ajax
             {
                 return false;
             }
-        }
-    }
-}
+        } // End Property  IsReusable
+
+
+    } // End Class serialize : IHttpHandler
+
+
+} // End Namespace LargeObjectSerializer.ajax
